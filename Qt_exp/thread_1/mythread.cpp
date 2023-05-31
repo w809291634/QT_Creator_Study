ef0898bfa163fda121e976ec191f95a94361f0f2
汪浩 <wanghao@zonesion.comc.cn>
2023-05-31 18:01:05 +0800

    gx

---
+++ Qt_exp/thread_1/mythread.cpp
@@ -0,0 +1,26 @@
+﻿#include "mythread.h"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MyThread::MyThread(QObject * parent):
+    QObject(parent)
+{
+    // execute signals in a new thread
+    thread = new QThread();
+    thread->start();
+    qDebug("HttpConnectionHandler (%p): thread started", static_cast<void*>(this));
+
+    moveToThread(thread);
+    readTimer.moveToThread(thread);
+    readTimer.setSingleShot(true);
+
+    connect(&readTimer, SIGNAL(timeout()), SLOT(readTimeout()));
+    connect(thread, SIGNAL(finished()), this, SLOT(thread_done()));
+}
+
+MyThread::~MyThread()
+{
+
+}
+
+
---
+++ Qt_exp/thread_1/mythread.h
@@ -0,0 +1,44 @@
+﻿#ifndef MYTHREAD_H
+#define MYTHREAD_H
+
+#include <QThread>
+#include <QtCore>
+
+class MainWindow;
+class MyThread : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyThread(QObject * parent=nullptr);
+    ~MyThread();
+
+    void set_callback(void (*cb)(void));
+    bool event(QEvent *event);
+
+signals:
+    void MsgSignal(const QString& msg);
+    void MsgSignal(const QString& act,
+                   const QString& cnt1,
+                   const QString& cnt2);
+
+private:
+    /** Configuration settings */
+    const QSettings* settings;
+
+    /** The thread that processes events of this connection */
+    QThread* thread;
+
+    /** Time for read timeout detection */
+    QTimer readTimer;
+
+private slots:
+
+    /** Received from the socket when a read-timeout occured */
+    void readTimeout();
+
+    /** Cleanup after the thread is closed */
+    void thread_done();
+};
+
+
+#endif // MYTHREAD_H
