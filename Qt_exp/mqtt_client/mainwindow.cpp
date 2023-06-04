07fd50b915739e195ae5b3aa0abfdf7f6f1f81b9
wanghao <809291634@qq.com>
2023-06-04 11:20:17 +0800

    gx

---
+++ Qt_exp/mqtt_client/mainwindow.cpp
@@ -0,0 +1,113 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDateTime>
+#include "QtMqtt/qmqttclient.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    m_client = new QMqttClient(this);
+    m_client->setHostname(ui->lineEditHost->text());
+    m_client->setPort(static_cast<quint16>(ui->spinBoxPort->value()));
+
+    connect(m_client, SIGNAL(stateChanged(ClientState)), this, SLOT(updateLogStateChange()));
+    connect(m_client, SIGNAL(disconnected()), this, SLOT(brokerDisconnected()));
+
+    connect(m_client, &QMqttClient::messageReceived, this, [this](const QByteArray &message, const QMqttTopicName &topic) {
+        const QString content = QDateTime::currentDateTime().toString()
+                    + QLatin1String(" Received Topic: ")
+                    + topic.name()
+                    + QLatin1String(" Message: ")
+                    + message
+                    + QLatin1Char('\n');
+        ui->editLog->insertPlainText(content);
+    });
+
+    connect(m_client, &QMqttClient::pingResponseReceived, this, [this]() {
+        ui->buttonPing->setEnabled(true);
+        const QString content = QDateTime::currentDateTime().toString()
+                    + QLatin1String(" PingResponse")
+                    + QLatin1Char('\n');
+        ui->editLog->insertPlainText(content);
+    });
+
+    connect(ui->lineEditHost, SIGNAL(textChanged(const QString)),
+            m_client, SLOT(setHostname(const QString)));
+    connect(ui->spinBoxPort, SIGNAL(valueChanged(int)), this, SLOT(setClientPort(int)));
+    updateLogStateChange();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+
+void MainWindow::on_buttonConnect_clicked()
+{
+    if (m_client->state() == QMqttClient::Disconnected) {
+        ui->lineEditHost->setEnabled(false);
+        ui->spinBoxPort->setEnabled(false);
+        ui->buttonConnect->setText(tr("Disconnect"));
+        m_client->connectToHost();
+    } else {
+        ui->lineEditHost->setEnabled(true);
+        ui->spinBoxPort->setEnabled(true);
+        ui->buttonConnect->setText(tr("Connect"));
+        m_client->disconnectFromHost();
+    }
+}
+
+void MainWindow::on_buttonQuit_clicked()
+{
+    QApplication::quit();
+}
+
+void MainWindow::updateLogStateChange()
+{
+    const QString content = QDateTime::currentDateTime().toString()
+                    + QLatin1String(": State Change")
+                    + QString::number(m_client->state())
+                    + QLatin1Char('\n');
+    ui->editLog->insertPlainText(content);
+}
+
+void MainWindow::brokerDisconnected()
+{
+    ui->lineEditHost->setEnabled(true);
+    ui->spinBoxPort->setEnabled(true);
+    ui->buttonConnect->setText(tr("Connect"));
+}
+
+void MainWindow::setClientPort(int p)
+{
+    m_client->setPort(static_cast<quint16>(p));
+}
+
+void MainWindow::on_buttonPublish_clicked()
+{
+    if (m_client->publish(ui->lineEditTopic->text(), ui->lineEditMessage->text().toUtf8()) == -1)
+        QMessageBox::critical(this, QLatin1String("Error"), QLatin1String("Could not publish message"));
+}
+
+void MainWindow::on_buttonSubscribe_clicked()
+{
+    auto subscription = m_client->subscribe(ui->lineEditTopic->text());
+    if (!subscription) {
+        QMessageBox::critical(this, QLatin1String("Error"), QLatin1String("Could not subscribe. Is there a valid connection?"));
+        return;
+    }
+}
+
+void MainWindow::on_buttonPing_clicked()
+{
+    ui->buttonPing->setEnabled(false);
+    m_client->requestPing();
+}
---
+++ Qt_exp/mqtt_client/mainwindow.h
@@ -0,0 +1,47 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1600)
+# pragma execution_character_set("utf-8")
+#endif
+
+#include <QtCore>
+#include <QMainWindow>
+#include "QtMqtt/qmqttclient.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void setClientPort(int p);
+
+private slots:
+    void on_buttonConnect_clicked();
+    void on_buttonQuit_clicked();
+    void updateLogStateChange();
+
+    void brokerDisconnected();
+
+    void on_buttonPublish_clicked();
+
+    void on_buttonSubscribe_clicked();
+
+    void on_buttonPing_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QMqttClient *m_client;
+};
+
+#endif // MAINWINDOW_H
