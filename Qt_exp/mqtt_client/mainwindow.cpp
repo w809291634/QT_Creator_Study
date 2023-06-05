80a7532daf0e37ec0e58d72e7b629dd615aca5d6
wanghao <809291634@qq.com>
2023-06-05 21:02:17 +0800

    gx

--- Qt_exp/mqtt_client/mainwindow.cpp
+++ Qt_exp/mqtt_client/mainwindow.cpp
@@ -12,7 +12,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     m_client = new QMqttClient(this);
-    m_client->setHostname(ui->lineEditHost->text());
+    m_client->setHostname(ui->host_cbox->currentText());
     m_client->setPort(static_cast<quint16>(ui->spinBoxPort->value()));
 
     connect(m_client, SIGNAL(stateChanged(ClientState)),
@@ -25,7 +25,7 @@ MainWindow::MainWindow(QWidget *parent) :
             this, SLOT(client_messageReceived(const QByteArray &, const QMqttTopicName &)));
     connect(m_client, SIGNAL(pingResponseReceived()),
             this,SLOT(client_ping_response()));
-    connect(ui->lineEditHost, SIGNAL(textChanged(const QString)),
+    connect(ui->host_cbox, SIGNAL(textChanged(const QString)),
             m_client, SLOT(setHostname(const QString)));
     connect(ui->spinBoxPort, SIGNAL(valueChanged(int)),
             this, SLOT(setClientPort(int)));
@@ -47,7 +47,6 @@ void MainWindow::client_messageReceived(
                 + QLatin1String(" Message: ")
                 + message
                 + QLatin1Char('\n');
-    ui->editLog->insertPlainText(content);
 }
 
 void MainWindow::client_ping_response()
@@ -56,19 +55,18 @@ void MainWindow::client_ping_response()
     const QString content = QDateTime::currentDateTime().toString()
                 + QLatin1String(" PingResponse")
                 + QLatin1Char('\n');
-    ui->editLog->insertPlainText(content);
 }
 
 
 void MainWindow::on_buttonConnect_clicked()
 {
     if (m_client->state() == QMqttClient::Disconnected) {
-        ui->lineEditHost->setEnabled(false);
+        ui->host_cbox->setEnabled(false);
         ui->spinBoxPort->setEnabled(false);
         ui->buttonConnect->setText(tr("Disconnect"));
         m_client->connectToHost();
     } else {
-        ui->lineEditHost->setEnabled(true);
+        ui->host_cbox->setEnabled(true);
         ui->spinBoxPort->setEnabled(true);
         ui->buttonConnect->setText(tr("Connect"));
         m_client->disconnectFromHost();
@@ -86,12 +84,12 @@ void MainWindow::updateLogStateChange()
                     + QLatin1String(": State Change")
                     + QString::number(m_client->state())
                     + QLatin1Char('\n');
-    ui->editLog->insertPlainText(content);
+//    ui->editLog->insertPlainText(content);
 }
 
 void MainWindow::brokerDisconnected()
 {
-    ui->lineEditHost->setEnabled(true);
+    ui->host_cbox->setEnabled(true);
     ui->spinBoxPort->setEnabled(true);
     ui->buttonConnect->setText(tr("Connect"));
 }
@@ -103,13 +101,13 @@ void MainWindow::setClientPort(int p)
 
 void MainWindow::on_buttonPublish_clicked()
 {
-    if (m_client->publish(ui->lineEditTopic->text(), ui->lineEditMessage->text().toUtf8()) == -1)
+    if (m_client->publish(ui->pub_topic_cbox->currentText(), ui->lineEditMessage->text().toUtf8()) == -1)
         QMessageBox::critical(this, QLatin1String("Error"), QLatin1String("Could not publish message"));
 }
 
 void MainWindow::on_buttonSubscribe_clicked()
 {
-    auto subscription = m_client->subscribe(ui->lineEditTopic->text());
+    auto subscription = m_client->subscribe(ui->sub_topic_cbox->currentText());
     if (!subscription) {
         QMessageBox::critical(this, QLatin1String("Error"), QLatin1String("Could not subscribe. Is there a valid connection?"));
         return;
